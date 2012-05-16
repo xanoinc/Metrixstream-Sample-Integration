87f96bef6d2a5b221545aa564e51555bcd706e4b
Sean Montgomery <sean@metrixapps.com>
2012-05-16 14:52:55 -0700

    example context

---
+++ backend/context/example.php
@@ -0,0 +1,42 @@
+<?
+  class ExampleContext extends BackendContext {
+  
+    public function getInternalName() {
+      return "example";
+    }
+  
+    public function getDisplayName() {
+      return "My Example Context";
+    }
+  
+    public function isDependentOnScope($scopeKey) {
+      return false;
+    }
+  
+    public function getConsumableParams() {
+      return Array("hello");
+    }
+  
+    public function filterParam($paramName, $paramValue, $whitelist = Array()) {
+      switch($paramName) {
+        case "hello": return $paramValue;
+      }
+      return null;
+    }
+  
+    public function &execute(ScopeContext &$Scope, PageContext &$Page) {
+      $App =& $this->getApplicationContext();
+      $Render =& $App->getRenderContext();
+  
+      $params = Array(
+        "hello" => $this->getParam($Page, "hello", "")
+      );
+  
+      $out =& $Render->getRenderOutput();
+      $out->setCacheable(true);
+      $out->getHDF()->set("hello", $params["hello"]);
+  
+      return $out;
+    }
+  }
+?>
